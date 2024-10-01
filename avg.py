229e2d891b82bd651ff1af20bb25aba5bb870b5f
Dilbur roshan mohamed <148055236+dilburroshanmohamed@users.noreply.github.com>
2024-10-01 07:57:59 +0100

    Add files via upload

---
+++ avg.py
@@ -0,0 +1,9 @@
+s1=int(input("enter first subject mark : "))
+s2=int(input("enter second subject mark : "))
+s3=int(input("enter third subject mark : "))
+s4=int(input("enter fourth subject mark : "))
+s5=int(input("enter fifth subject mark : "))
+total = s1+s2+s3+s4+s5
+print("sum = ",total)
+avg =total/5
+print("average = ", avg)
