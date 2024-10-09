d01eafc75be2bb6c89c521412c51d0dafe01de83
Dilbur roshan mohamed <148055236+dilburroshanmohamed@users.noreply.github.com>
2024-10-09 10:24:09 +0530

    Update leap.py

--- week1/leap.py
+++ week1/leap.py
@@ -7,7 +7,7 @@ def is_leap_year(year):
 start_year = 2024
 end_year = int(input("Enter the end year: "))
 
-print("Leap years between", start_year , end_year,"are:")
+print("Leap years between", start_year,",", end_year,"are:")
 for year in range(start_year, end_year + 1):
     if is_leap_year(year):
         print(year)
