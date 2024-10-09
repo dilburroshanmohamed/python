0fd8405d984ab83d1505c81a25e057491b9b89fa
Dilbur roshan mohamed <148055236+dilburroshanmohamed@users.noreply.github.com>
2024-10-09 10:05:32 +0530

    Update square.py

--- week1/square.py
+++ week1/square.py
@@ -1,4 +1,4 @@
-n = int(input("Enter the nth number in the series:"))
+n = int(input("Enter the last number in the series:"))
 i = 1
 print("The squares of the first" , n , "numbers is:")
 while(i <= n):
