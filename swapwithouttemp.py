8f72ea2a0dc1cd66470638924bd21ee64db9cbf7
Dilbur roshan mohamed <148055236+dilburroshanmohamed@users.noreply.github.com>
2024-10-18 15:23:18 +0530

    Update swapwithouttemp.py

--- swapwithouttemp.py
+++ swapwithouttemp.py
@@ -1,5 +1,5 @@
-num1=int(input("enter num1"))
-num2=int(input("enter num2"))
+num1=int(input("enter num1 :"))
+num2=int(input("enter num2 :"))
 print(f"before swapping in num1={num1}and num2={num2}")
 num1+=num2
 num2=num1-num2
