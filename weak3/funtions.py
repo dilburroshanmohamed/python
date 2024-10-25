17348ed5728f39b0388dee735d83d17a2db7480a
Dilbur roshan mohamed <148055236+dilburroshanmohamed@users.noreply.github.com>
2024-10-25 15:23:01 +0530

    Add files via upload

---
+++ weak3/funtions.py
@@ -0,0 +1,14 @@
+li=[55,54,22,22,66,66,27]
+print(li)
+m=min(li)
+x=max(li)
+sort=sorted(li)
+count=len(li)
+sum=sum(li)
+
+print("minimum value of the list is : ",m)
+print("maximum value of the list is : ",x)
+print("sorted  list is : ",sort)
+print("length of list is : ",count)
+print("sum = ",sum)
+
