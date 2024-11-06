3fc813992df91820e55dbe5a822dedb8f09a1167
Dilbur roshan mohamed <148055236+dilburroshanmohamed@users.noreply.github.com>
2024-11-06 16:03:04 +0530

    Delete weak2 directory

--- weak2/oddeven.py
+++
@@ -1,7 +0,0 @@
-n = int(input("Enter the number: "))
-
-if (n % 2) == 0:
-    print("The number", n, "is even")
-else:
-    print("The number", n, "is odd")
-
--- weak2/vowels.py
+++
@@ -1,5 +0,0 @@
-text = input('Enter text: ')
-print("The vowels are:")
-for letter in text:
-    if letter in 'aeiouAEIOU':  
-        print(letter)
