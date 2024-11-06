16db25b25531959ade8fbd3f9bd1acd20370fc84
Dilbur roshan mohamed <148055236+dilburroshanmohamed@users.noreply.github.com>
2024-11-06 16:06:54 +0530

    Add files via upload

---
+++ week2/factorial.py
@@ -0,0 +1,5 @@
+n=int(input('enter the number: '))
+sum=1
+for i in range(1,n+1,1):
+ sum=sum*i
+print("factorial of ",n,"is",sum)
---
+++ week2/vowels.py
@@ -0,0 +1,9 @@
+text = input('Enter text: ')
+print("The vowels are:")
+count = 0
+for letter in text:
+    if letter in 'aeiouAEIOU':
+        count += 1
+print(f"Number of vowels: {count}")
+        print(letter)
+
