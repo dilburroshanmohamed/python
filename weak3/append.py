f7c0634e9f062612ca0436358b2ca9f7ed18e722
Dilbur roshan mohamed <148055236+dilburroshanmohamed@users.noreply.github.com>
2024-11-06 16:02:42 +0530

    Delete weak3 directory

--- weak3/append.py
+++
@@ -1,8 +0,0 @@
-fruits = [ ]
-print(fruits);
-
-n=int(input("enter the size : "));
-for i in range(0,n):
- e=input(f"Enter fruit {i + 1}: ");
- fruits.append(e)
-print(fruits);
--- weak3/fist_last_color.py
+++
@@ -1,5 +0,0 @@
-
-colors = input("Enter comma-separated color names: ").split(",")
-
-print("First color:", colors[0])
-print("Last color:", colors[-1])
