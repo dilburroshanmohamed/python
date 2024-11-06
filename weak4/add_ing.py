c669d998c71a3ac01fe5411e62e3e158a012d654
Dilbur roshan mohamed <148055236+dilburroshanmohamed@users.noreply.github.com>
2024-11-06 16:02:54 +0530

    Delete weak4 directory

--- weak4/add_ing.py
+++
@@ -1,8 +0,0 @@
-strr = input("Enter a string:")
-leng = len(strr)
-diff = leng-4
-if strr[-3:]=="ing":
-    strr=strr+"ly"
-else:
-    strr=strr+"ing"
-print(f"{strr}")
--- weak4/longest_word.py
+++
@@ -1,9 +0,0 @@
-size = int(input("Enter the size of the list:"))
-lst = []
-print(f"Enter {size} strings in the list:")
-for i in range(size):
-    strr = input()
-    lst.append(strr)
-maxx = max(lst, key=len)
-print(f"List items:{lst}\n")
-print(f"Longest value in the list:{maxx}")
