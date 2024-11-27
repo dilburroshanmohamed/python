543711668df286393a3d5b43eb9689b258ad7d74
Dilbur roshan mohamed <148055236+dilburroshanmohamed@users.noreply.github.com>
2024-11-27 11:54:38 +0530

    Add files via upload

---
+++ math/geomatric_operations/area.py
@@ -0,0 +1,8 @@
+#area.py
+def rect_area(length,width):
+        return length*width
+def circle_area(radius):
+        from math import pi
+        return radius*radius
+def triangle_area(base,height):
+        return base*height        
